bfef40ec3c6679183eec11a190186f9d7c50e428
art.antoshin <art.antoshin1992@gmail.com>
2022-10-14 02:27:07 +0400

    Добавьте файлы проекта.

---
+++ InterFaceAction/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaceAction
+{
+    // применение интерфейса в классе
+    class Person : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Человек идет");
+        }
+    }
+    // применение интерфейса в структуре
+    struct Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Машина едет");
+        }
+    }
+}
---
+++ InterFaceAction/Program.cs
@@ -0,0 +1,8 @@
+﻿using InterFaceAction;
+
+Person person = new Person();
+Car car = new Car();
+DoAction(person);
+DoAction(car);
+
+void DoAction(IMovable movable) => movable.Move();
