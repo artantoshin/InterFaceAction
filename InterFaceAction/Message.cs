e706d61c9318a5a64ae00e4df1a741cbfde3e918
art.antoshin <art.antoshin1992@gmail.com>
2022-10-14 15:49:20 +0400

    Множественная реализация интерфейсов + преобразование типов

---
+++ InterFaceAction/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaceAction
+{
+    internal class Message: IMessage,IPrintable
+    {
+        public string Text { get; set; }
+        public Message(string text) => Text = text;
+        public void Print() => Console.WriteLine(Text);
+    }
+}
--- InterFaceAction/Program.cs
+++ InterFaceAction/Program.cs
@@ -1,8 +1,20 @@
 ﻿using InterFaceAction;
+using System.Text;
+
+var enc65001 = Encoding.GetEncoding(65001);
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = enc65001;
 
 Person person = new Person();
 Car car = new Car();
 DoAction(person);
 DoAction(car);
+
+Message hello = new Message("Hello World");
+hello.Print();  // Hello World
+
+// Все объекты Message являются объектами IMessage
+IMessage hello2 = new Message("Hello METANIT.COM");
+Console.WriteLine(hello2.Text); // Hello METANIT.COM
 
 void DoAction(IMovable movable) => movable.Move();
